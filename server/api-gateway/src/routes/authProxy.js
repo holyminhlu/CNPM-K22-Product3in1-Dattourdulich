17ef609e3d4e03adecfab8e6a0d44f0a4738585c
nguyenhuuluanTTB <nguyenhuuluan19092004@gmail.com>
2025-07-17 00:21:51 +0700

    Xây dựng service quản lý đăng nhập, đăng ký tài khoản, xử lý quên mật khẩu cùng gateway-api

---
+++ server/api-gateway/src/routes/authProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+  pathRewrite: { '^/api/auth': '' }
+});
