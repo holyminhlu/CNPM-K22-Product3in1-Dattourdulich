804c262ecdd618c6691fc8b59909f48173237123
huynhkhaitvu <huynhkhai2062@gmail.com>
2025-07-17 21:46:52 +0700

    them asset main app

---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'font-awesome/css/font-awesome.css';
+import router from './routes';
+import 'aos/dist/aos.css'; // Import CSS AOS
+import AOS from 'aos';     // Import thư viện JS
+import gsap from 'gsap';
+
+
+createApp(App).use(router).mount('#app')
+AOS.init();
+gsap.to("body", { backgroundColor: "#fff", duration: 1 });
