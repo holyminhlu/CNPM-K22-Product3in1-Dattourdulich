1c0abfbe09e63ad5558545dfcfcc1ea2e91f030f
minhlu <holyminhlu@gmail.com>
2025-07-22 23:27:14 +0700

    Nâng cấp trang Home

--- client/cheaptripfe/src/views/Articles/HomePage.vue
+++ client/cheaptripfe/src/views/Articles/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Trang Chủ</h1>
-    <p>Chào mừng đến với CheapTrip!</p>
+    <p>Chào mừng đến với web đặt tour du lịch nghĩ dưỡng CheapTrip!</p>
   </div>
 </template>
 
